5a2ac9c4520c927b117a9f637d39f622b5738422
Kanishk <kanishkchahar@gmail.com>
2026-07-06 12:53:26 +0530

    Milestone 4: Implement prediction REST API

--- predictor/views.py
+++ predictor/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from inference import predict
+from .models import Prediction
+
+
+class PredictAPIView(APIView):
+
+    def post(self, request):
+
+        result = predict(request.data)
+
+        Prediction.objects.create(
+            inputs=request.data,
+            prediction=result["prediction"],
+            label=result["label"],
+            probability=result["probability"]
+        )
+
+        return Response(result, status=status.HTTP_200_OK)
